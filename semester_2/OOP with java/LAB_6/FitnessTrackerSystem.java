a1ca2ff3e26dd8fd71e36f4b0d2d6ae6942275a2
Kamran Gul <kamrangul42142@gmail.com>
2026-03-22 12:11:55 +0500

    Lab 6: Methods and Main class added

--- semester_2/OOP with java/LAB_6/FitnessTrackerSystem.java
+++ semester_2/OOP with java/LAB_6/FitnessTrackerSystem.java
@@ -38,17 +38,89 @@ class FitnessTracker{
 		workoutMinutes += min;
 	}
 	void recordWorkout(int min){
-		workoutMinutes += min;
-	}
+		weeklySteps = new int[]{100,456,345,321,43,768,432}; 
+			}
 	double[] getDailyProgress(){
-		double stepsGoal = (dailySteps/goalSteps) * 100;
+		double stepsGoal = (dailySteps/(double)goalSteps) * 100;
 		double caloriesGoal = (dailyCaloriesBurned/goalCalories) * 100;
 
 		return new double[]{stepsGoal,caloriesGoal};
 	}
+	int getBestDay(){
+		int highest = weeklySteps[0];
+		int day = 0;
+		for(int i=1; i<weeklySteps.length;i++){
+			if(weeklySteps[i]>highest){
+				highest = weeklySteps[i];
+				day =i;
+			}
+		}
+		return day;
+	}
+	boolean isGoalAchieved(){
+		if(dailySteps>=goalSteps && dailyCaloriesBurned>=goalCalories){
+			return true;
+		}
+		else 
+			return false;
+	}
+	void resetDailyStats(){
+		this.dailySteps = 0;
+		this.dailyCaloriesBurned = 0;
+		this.workoutMinutes =0;
+	}
+	String getFitnessReport(){
+		return String.format("User Name: %s\nDaily Steps: %d\nCalories Burned: %.2f\nTotal workout Minutes: %d\n",userName,dailySteps,dailyCaloriesBurned,workoutMinutes);
+	}
+
 }
 class Main{
 	public static void main(String[] args){
+		FitnessTracker tracker1 = new FitnessTracker("Kamran",2200,6730);
 
+		// 2. Add steps throughout the day
+        tracker1.addSteps(1000);
+        tracker1.addSteps(1500); // Total steps: 2500
+
+        // 3. Record different types of workouts & 4. Calculate calories burned
+        tracker1.recordWorkout(45, "Running");   // 45 * 10 = 450 cals
+        tracker1.recordWorkout(30, "Swimming");  // 30 * 12 = 360 cals
+
+        // 5. Track weekly progress (Using your dummy data method)
+        tracker1.recordWorkout(0); // Calling the overloaded method to populate the dummy array
+        System.out.println("Best day of the week (Index 0-6): Day " + tracker1.getBestDay());
+
+        // 6. Check if goals are achieved
+        System.out.println("Are Kamran's goals achieved? " + tracker1.isGoalAchieved());
+
+        // 7. Generate fitness report
+        System.out.println("\nKamran's Report:");
+        System.out.println(tracker1.getFitnessReport());
+
+
+        System.out.println("\n--- COMPARING TWO USERS ---");
+        
+        // 8. Compare progress between two users
+        FitnessTracker tracker2 = new FitnessTracker("Sara", 5000, 1000);
+        
+        // Add some data for Sara
+        tracker2.addSteps(6000);
+        tracker2.recordWorkout(60, "Cycling"); 
+        
+        System.out.println("Sara's Report:");
+        System.out.println(tracker2.getFitnessReport());
+        
+        // Perform the comparison
+        System.out.println("--- Comparison Results ---");
+        if (tracker1.calculateCaloriesBurned(720,"Swimming") > tracker2.calculateCaloriesBurned(600,"Cycling")) {
+            System.out.println("Kamran burned more calories today!");
+        } else if (tracker2.calculateCaloriesBurned(600,"Cycling") > tracker1.calculateCaloriesBurned(720,"Swimming")) {
+            System.out.println("Sara burned more calories today!");
+        } else {
+            System.out.println("Kamran and Sara burned the exact same amount of calories!");
+        }
+        
+        System.out.println("Kamran Goal Achieved: " + tracker1.isGoalAchieved());
+        System.out.println("Sara Goal Achieved: " + tracker2.isGoalAchieved());
 	}
 }
