5f0b1d11d74550bef384de95a5f6c86aaedabec0
Kamran Gul <kamrangul42142@gmail.com>
2026-09-05 22:40:33 +0500

    Simple LL code added (with tail)

---
+++ semester_3/DSA/SimpleLinkedList_WithTail.java
@@ -0,0 +1,204 @@
+class Node{
+	int data;
+	Node next;
+	Node(int data){
+		this.data = data;
+		this.next = null;
+	}
+}
+class SimpleLinkedList{
+	Node head;
+	Node tail;
+	SimpleLinkedList(){
+		this.head = null;
+		this.tail = null;
+	}
+	void addFront(int data){
+		Node newNode = new Node(data);
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+			return;
+		}
+		newNode.next = head;
+		head = newNode;
+	}
+	void addBack(int data){
+		Node newNode = new Node(data);
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+			return;
+		}
+		tail.next = newNode;
+		tail = newNode;	
+	}
+	void deleteFront(){
+		if(head == null){
+			System.out.print("List is empty, Nothing to delete");
+			return;
+		}
+		head = head.next;
+	}
+	void deleteBack(){
+		if(head == null){
+			System.out.print("List is empty, Nothing to delete");
+			return;
+		}
+		if(head.next == null){ //if the list have only one node this will handle it
+			head = null;
+			tail = null;
+			return;
+		}
+		Node current = head;
+		while(current.next.next != null){
+			current = current.next;
+		}
+		current.next = null;
+		tail = current;
+	}
+	void delete(int target){ //till here
+		if(head == null){
+			System.out.print("List is empty, Nothing to delete");
+			return;
+		}
+		if(head.data == target){
+			head = head.next;
+			return;
+		}
+		Node current = head;
+		while(current.next.data != target){
+			current = current.next;
+			if(current.next == null){ //if the targeted node is not in the list
+				return;
+			}
+		}
+		if(current.next.data == tail.data){
+			tail = current;
+		}
+		current.next = current.next.next;
+	}
+	void addBefore(int target,int data){
+		if(target == head.data){
+			addFront(data);
+			return;
+		}
+		Node current = head;
+		while(current.next.data != target){
+			current = current.next;
+			if(current.next == null){ //if the targeted node is not in the list
+				return;
+			}
+		}
+		Node newNode = new Node(data);
+		newNode.next = current.next;
+		current.next = newNode;
+	}
+	void addAfter(int target, int data){
+		Node newNode = new Node(data);
+		Node current = head;
+		while(current.data != target){
+			current = current.next;
+			if(current == null){
+				return;
+			}
+		}
+		if(current == tail){
+			newNode.next = current.next;
+			current.next = newNode;
+			tail = newNode;
+			return;
+		}
+		newNode.next = current.next;
+		current.next = newNode;
+	}
+	void deleteBefore(int target){
+		if(head == null){
+			System.out.print("The list is empty, nothing to delete");
+			return;
+		}
+		if(head.next.data == target){
+			head = head.next;
+			return;
+		}
+		Node current = head;
+		while(current.next.next.data != target){
+			current = current.next;
+			if(current.next.next == null){
+				return;
+			}
+		}
+		current.next = current.next.next;
+	}
+	void deleteAfter(int target){
+		if(head == null){
+			System.out.print("The list is empty, Nothing to delete");
+			return;
+		}
+		Node current = head;
+		while(current.data != target){
+			current = current.next;
+			if(current.next == null){
+				return;
+			}
+		}
+		if(current.next == tail){
+			tail = current;
+			current.next = null;
+			return;
+		}
+		current.next = current.next.next;
+	}
+	void search(int target){
+		int count = 1; //This is bcz when data is found the loop doesnt count that node
+		Node current = head;
+		while(current.data != target){
+			count+=1;
+			current = current.next;
+			if(current == null){
+				return;
+			}
+		}
+		System.out.print("Data found at node: " + count);
+	}
+	void update(int target, int newData){
+		Node current = head;
+		while(current.data != target){
+			current = current.next;
+			if(current == null){
+				return;
+			}
+		}
+		current.data = newData;
+	}
+	void display(){
+		if(head == null){
+			System.out.print("\nList is empty, Nothing to display\n");
+			return;
+		}
+		System.out.print("\nThe head is: "+ head.data);
+		System.out.print("\nThe tail is: "+ tail.data + "\n\n");
+		Node current = head;
+		while(current!=null){
+			System.out.print(current.data + " ");
+			current = current.next;
+		}
+		System.out.print("\n\n");
+	}
+}
+class Main{
+	public static void main(String[] args){
+		SimpleLinkedList list = new SimpleLinkedList();
+		list.addFront(5);
+		list.addFront(4);
+		list.addFront(3);
+		list.addFront(2);
+		list.addFront(1);
+		// list.addBack(500);
+		// list.deleteBack();
+		// list.deleteFront();
+		// list.addAfter(5,100);
+		// list.deleteAfter(4);
+		list.display();
+	}
+}
